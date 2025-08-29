da0f6d14537e1a0dbd3f68eb96a939dab77528f6
Mohith <mohithlally@gmail.com>
2025-08-29 11:47:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Mohith's Portfolio");
+
+function toggleDescription(card) {
+    card.classList.toggle("active");
+}
+
+function toggleProject(card) {
+    card.classList.toggle("active");
+}
